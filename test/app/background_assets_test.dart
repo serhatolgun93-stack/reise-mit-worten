516b369467616fecdcf18c8dba5201104d772723
serhatolgun93-stack <serhat.olgun.93@gmail.com>
2026-08-17 00:22:14 +0200

    fix: update Santorini background dimensions

--- test/app/background_assets_test.dart
+++ test/app/background_assets_test.dart
@@ -9,7 +9,7 @@ void main() {
   const approved = <String, ({int width, int height, int minBytes})>{
     'assets/backgrounds/english_london.png': (width: 467, height: 720, minBytes: 100000),
     'assets/backgrounds/turkish_istanbul.png': (width: 720, height: 480, minBytes: 100000),
-    'assets/backgrounds/greek_santorini.png': (width: 256, height: 256, minBytes: 30000),
+    'assets/backgrounds/greek_santorini.png': (width: 1600, height: 1066, minBytes: 30000),
   };
 
   for (final entry in approved.entries) {
